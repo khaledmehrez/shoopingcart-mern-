5f57521cacb99f6196600d8ad2e322d629fd53ee
khaledsimplon <khaledmehrezsimplon@gmail.com>
2021-01-12 20:05:12 +0100

    add react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <div class="grid-container">
+      <header class="row">
+        <div>
+          <a class="brand" href="index.html">amazona</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      </div>
     </div>
   );
 }
