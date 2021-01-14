e81d6476e7f1ca1d15fd2ea6d12cd7e5a1a7e47d
khaledsimplon <khaledmehrezsimplon@gmail.com>
2021-01-14 15:38:09 +0100

    add product screen and home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
+    
     <div className="App">
+      <BrowserRouter>
       <div class="grid-container">
       <header class="row">
         <div>
@@ -15,7 +20,14 @@ function App() {
         </div>
       </header>
       </div>
+      <main>
+        <Route path="/" component={HomeScreen} exact="/"/>
+       <Route path="/product/:id" component={ProductScreen}/>
+     
+    </main>
+    </BrowserRouter>
     </div>
+    
   );
 }
 
---
+++ frontend/src/screens/NotfoundScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotfoundScreen() {
+    return (
+        <div>
+<img src="/images/notfound.jpg" alt="notfound"/>
+        </div>
+    )
+}
